24d58457e522d6d001ca70eac7d5f184f44d0c97
Cristiano  Gomes <cristiano1981gomes@gmail.com>
2023-06-01 20:16:24 -0300

    testando de novo

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-const whatsappButton = document.getElementById('whatsapp-button');
-
+const whatsappButton = document.querySelector('.whatsapp-button');
 
 whatsappButton.addEventListener('click', () => {
- 
-  const phoneNumber = '55 11 985508341';
+    const phoneNumber = '55 11 985508341';
+    const message = 'Olá, gostaria de fazer um pedido.';
+    const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
 
- 
-  const message = 'Olá, gostaria de fazer um pedido.';
-
- 
-  const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
-
-  
-  window.location.href = whatsappURL;
+    window.location.href = whatsappURL;
 });
+
