44ae6087d3e1372a43093e9ce5244969e4c89717
Cristiano  Gomes <cristiano1981gomes@gmail.com>
2023-06-01 23:17:12 -0300

    botão do instagram

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ whatsappButton.addEventListener('click', () => {
     const phoneNumber = '55 11 985508341';
     const message = 'Olá, gostaria de fazer um pedido.';
     const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
-
-    window.location.href = whatsappURL;
+    window.open(whatsappURL, '_blank');
+    // window.location.href = whatsappURL;
 });
 
+
+const instagramButton = document.querySelector('.instagram-button');
+
+instagramButton.addEventListener('click', () => {
+    redirectToInstagram();
+});
+
+function redirectToInstagram() {
+    const instagramURL = 'https://www.instagram.com/cristiano.gomes17/';
+    window.open(instagramURL, '_blank');
+// window.open(instagramURL, '_blank');
+}
